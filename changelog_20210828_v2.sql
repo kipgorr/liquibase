70ce6d93527795f6b7b10f1d44baa874e3bed9f6
kipgorr <ikipgorr@gmail.com>
2021-09-02 12:02:37 -0400

    Update changelog_20210828_v2.sql
    TEST Prod

--- changelog_20210828_v2.sql
+++ changelog_20210828_v2.sql
@@ -1,8 +1,8 @@
 -- liquibase formatted sql
 
--- changeset dev.cicd:v5.1
--- rollback drop tabel person_cicd2_test_v5
-create table person_cicd2_test_v5 (
+-- changeset prod.cicd:v1.1
+-- rollback drop tabel person_cicd2_test
+create table person_cicd2_test (
     id_a int primary key,
     name varchar(50) not null,
     address1 varchar(50),
@@ -10,9 +10,9 @@ create table person_cicd2_test_v5 (
     city_a varchar(30)
 )
 
--- changeset dev.cicd:v5.2
--- rollback drop tabel company_cicd2_test_v5
-create table company_cicd2_test_v5 (
+-- changeset prod.cicd:v1.2
+-- rollback drop tabel company_cicd2_test
+create table company_cicd2_test (
     id_a int primary key,
     name varchar(50) not null,
     address1 varchar(50),
