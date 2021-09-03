4508f30aa54e33b2273226ad9eee3610703ecd79
kipgorr <ikipgorr@gmail.com>
2021-09-03 12:57:50 -0400

    Update changelog_20210828_v2.sql

--- changelog_20210828_v2.sql
+++ changelog_20210828_v2.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset prod_1.ci_cd:v2.1
+-- changeset prod_1.ci_cd:v3.1
 -- rollback drop tabel person_cicd2_t1
 create or replace table person_cicd2_t1 (
     id_a int primary key,
@@ -10,7 +10,7 @@ create or replace table person_cicd2_t1 (
     city_a varchar(30)
 )
 
--- changeset prod_1.ci_cd:v2.2
+-- changeset prod_1.ci_cd:v3.2
 -- rollback drop tabel company_cicd2_t1
 create or replace table company_cicd2_t1 (
     id_a int primary key,
